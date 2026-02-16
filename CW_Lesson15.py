bc4b6d0fbaafa500dc616ff62d2dd8236be7757b
Mariia <mariiaikonnikova6@gmail.com>
2026-02-16 09:58:39 +0200

    Add files via upload

--- CW_Lesson15.py
+++ CW_Lesson15.py
@@ -3,58 +3,49 @@ import cv2
 import time
 from ultralytics import YOLO
 
-
 PROJECT_DIR = os.path.dirname(__file__)
-
 VIDEO_DIR = os.path.join(PROJECT_DIR, 'video')
 OUT_DIR = os.path.join(PROJECT_DIR, 'out')
 
-os.makedirs(OUT_DIR, exist_ok=True)
+os.makedirs(OUT_DIR, exist_ok = True)
 
 
 USE_WEBCAM = True
 
 if USE_WEBCAM:
     cap = cv2.VideoCapture(0)
-
 else:
-    VIDEO_PATH = os.path.join(VIDEO_DIR, 'name')
+    VIDEO_PATH = os.path.join(VIDEO_DIR, "name")
     cap = cv2.VideoCapture(VIDEO_PATH)
 
 
 model = YOLO('yolov8n.pt')
 
-CONFIDENCE_THRESHOLD = 0.5
+CONF_TRESHOLD = 0.4
 
-RESIZE_WIDTH = 960 #None
-
+RESIZE_WIDTH = 960
 
 prev_time = time.time()
-fps = 0.0
+FPS = 0.0
+
 while True:
     ret, frame = cap.read()
-
     if not ret:
         break
 
-
     if RESIZE_WIDTH is not None:
         h, w = frame.shape[:2]
-
         scale = RESIZE_WIDTH / w
-
-        new_w = int(scale * w)
-        new_h = int(scale * h)
-
+        new_w = int(w * scale)
+        new_h = int(h * scale)
         frame = cv2.resize(frame, (new_w, new_h))
 
-
-    result = model(frame, conf = CONFIDENCE_THRESHOLD, verbose = False)
+    result = model(frame, conf = CONF_TRESHOLD, verbose = False)
 
     people_count = 0
     psevdo_id = 0
 
-    PERSON_CLASS_ID = 0
+    PERSONS_CLASS_ID = 0
 
     for r in result:
         boxes = r.boxes
@@ -64,19 +55,17 @@ while True:
         for box in boxes:
             cls = int(box.cls[0])
             conf = float(box.conf[0])
-
             x1, y1, x2, y2 = map(int, box.xyxy[0])
 
-            print(1)
-            if cls == PERSON_CLASS_ID:
-                print(2)
+
+            if cls == PERSONS_CLASS_ID:
                 people_count += 1
                 psevdo_id += 1
 
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-                label = f'ID: {psevdo_id} conf {conf:.2f}'
-                cv2.putText(frame, label, (x1, max(20,y1-10)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 1)
+                label = f'ID {psevdo_id} conf {conf:.2f}'
+                cv2.putText(frame, label, (x1, max(20, y1-10)), cv2.FONT_HERSHEY_PLAIN, 0.6, (0, 255, 0), 2)
 
 
                 now = time.time()
@@ -86,13 +75,14 @@ while True:
                 if dt > 0:
                     fps = 1.0 / dt
 
-                cv2.putText(frame, f'People count: {people_count}', (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 1)
-                cv2.putText(frame, f'FPS: {fps:.1f}', (20, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 1)
+                cv2.putText(frame, f'People cpunt: {people_count}', (20, 40), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2)
+                cv2.putText(frame, f'FPS: {fps}', (20, 80), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2)
 
-    cv2.imshow('YOLO', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
 
+                cv2.imshow("YOLO", frame)
+
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    break
 cv2.destroyAllWindows()
 
 
